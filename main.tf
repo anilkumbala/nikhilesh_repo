f469788d3ffdb93e1fb84a3cf1d2c30b4071a1df
chintuawsdev <chintuawsdev@gmail.com>
2022-06-30 15:11:49 +0530

    Code changes for main

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     aws = {
       version = "<= 3.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
+      source = "gg/aws"
     }
   }
 }
